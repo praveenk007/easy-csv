8a78ffc70ef569b59129c59b9bf092f6d74d3c76
pk1992 <praveen.k@gmail.com>
2018-05-26 12:40:33 +0530

    moved test code to separate file

--- src/utils/TurtleCSV.java
+++ src/utils/TurtleCSV.java
@@ -2,11 +2,6 @@ package utils;
 
 import annotations.CSVHeader;
 import annotations.CSVHeaderPosition;
-import tests.Member;
-import tests.PolicyDetails;
-
-import java.io.File;
-import java.io.FileWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -42,7 +37,7 @@ public class TurtleCSV {
                 sb.append(Arrays.stream(fields).map(f -> "\"" + getFieldVal(f, obj, sb) +"\"").collect(Collectors.joining(del)));
                 sb.append(NEW_LINE);
             } catch (Exception e) {
-
+                e.printStackTrace();
             }
         } return Base64.getEncoder().encodeToString(sb.toString().getBytes());
     }
@@ -95,72 +90,6 @@ public class TurtleCSV {
                 return 0;
             }
         });
-    }
-
-    public static void main(String args[]) {
-        TurtleCSV t = new TurtleCSV(",");
-
-        PolicyDetails pd1 = new PolicyDetails();
-        pd1.setfName("praveen");
-        pd1.setlName("kamath");
-        pd1.setPolicyNum("12349494");
-        pd1.setAge(20);
-        List<String> previousPolicies = new ArrayList<>();
-        previousPolicies.add("pol1");
-        previousPolicies.add("pol2");
-        previousPolicies.add("pol3");
-        previousPolicies.add("pol4");
-        pd1.setPreviousPolicies(previousPolicies);
-        List<Integer> ages = new ArrayList<>();
-        ages.add(10);
-        ages.add(20);
-        ages.add(30);
-        Member m1 = new Member();
-        m1.setFname("praveen");
-        m1.setLname("kamath");
-        List<Member> members = new ArrayList();
-        pd1.setMemebers(members);
-        pd1.setAges(ages);
-
-        List<Double> policyPremiums = new ArrayList<>();
-        policyPremiums.add(10.0);
-        policyPremiums.add(20.1);
-        policyPremiums.add(30.2);
-        pd1.setPolicyPremiums(policyPremiums);
-
-        PolicyDetails pd2 = new PolicyDetails();
-        pd2.setfName("Beer");
-        pd2.setlName("Pratap");
-        pd2.setPolicyNum("47474747");
-        pd2.setAge(23);
-        List<String> previousPolicies2 = new ArrayList<>();
-        previousPolicies2.add("pol1");
-        previousPolicies2.add("pol2");
-        previousPolicies2.add("pol3");
-        previousPolicies2.add("pol4");
-        pd2.setPreviousPolicies(previousPolicies2);
-
-
-        List list = new ArrayList();
-        list.add(pd1);
-        //list.add(pd2);
-        byte[] b = Base64.getDecoder().decode(t.write(list, true));
-        FileWriter fw = null;
-        try {
-            fw = new FileWriter(new File("/Users/praveenkamath/Documents/test.csv"));
-            fw.write(new String(b));
-            fw.flush();
-            System.out.println("File written");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fw.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(new String(b));
     }
 }
 
